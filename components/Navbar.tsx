c0a71bce3e081ee749e867f87541519261b5e2f8
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-04-18 02:20:00 +0530

    navbar and mobile view

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import MobileNav from "./MobileNav";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="Let's Meet Logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Let's Meet
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        {/* Clerk User Management */}
+
+        {/* MobileNav */}
+        <MobileNav />
+      </div>
+    </nav>
+  );
 }
