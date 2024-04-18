0a5d9d7911d897a4f6d9a135f9e1cc429b63257f
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-04-19 00:29:18 +0530

    completed meeting setup

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function MeetingRoom() {
-  return <div></div>;
+  return <div>MeetingRoom</div>;
 }
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,5 +1,54 @@
-import React from "react";
+"use client";
+import {
+  DeviceSettings,
+  VideoPreview,
+  useCall,
+} from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
-export default function MeetingSetup() {
-  return <div></div>;
+export default function MeetingSetup({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void;
+}) {
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
+  const call = useCall();
+  if (!call)
+    throw new Error("useCall must be used within StreamCall component");
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToggledOn, call?.camera, call?.microphone]);
+  return (
+    <div className="flex flex-col justify-center items-center gap-3 text-white h-screen w-full">
+      <h1 className="font-bold text-2xl">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn}
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+          />
+          Join with Camera and Microphone Enabled
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        onClick={() => {
+          call.join();
+          setIsSetupComplete(true);
+        }}
+        className="rounded-md bg-green-500 px-4 py-2.5"
+      >
+        Join Meeting
+      </Button>
+    </div>
+  );
 }
