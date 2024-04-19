728dd4e6aa5e0034c497d9de7b599777e0f66be2
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-04-19 16:54:42 +0530

    Personal Room page

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -81,7 +81,7 @@ export default function CallList({
             title={
               (meeting as Call).state?.custom?.description ||
               (meeting as CallRecording).filename?.substring(0, 20) ||
-              "No Description"
+              "Personal Meeting"
             }
             date={
               (meeting as Call).state?.startsAt?.toLocaleString() ||
