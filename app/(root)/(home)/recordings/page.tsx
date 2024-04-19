46e42f3b2ad00a0f132433d36d826228ae5d6b99
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-04-19 16:31:09 +0530

    Recordings page

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,9 +1,11 @@
+import CallList from "@/components/CallList";
 import React from "react";
 
 export default function Recordings() {
   return (
     <section className="flex text-white size-full flex-col gap-10">
       <h1 className="text-3xl font-bold">Recordings</h1>
+      <CallList type="recordings" />
     </section>
   );
 }
--- components/CallList.tsx
+++ components/CallList.tsx
@@ -2,9 +2,10 @@
 import { useGetCalls } from "@/hooks/useGetCalls";
 import { Call, CallRecording } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MeetingCard from "./MeetingCard";
 import Loader from "./Loader";
+import { useToast } from "./ui/use-toast";
 
 export default function CallList({
   type,
@@ -13,6 +14,7 @@ export default function CallList({
 }) {
   const { endedCalls, upcomingCalls, callRecordings, isLoading } =
     useGetCalls();
+  const { toast } = useToast();
   const [recordings, setRecordings] = useState<CallRecording[]>([]);
   const router = useRouter();
   const getCalls = () => {
@@ -39,6 +41,27 @@ export default function CallList({
         return "";
     }
   };
+  useEffect(() => {
+    try {
+      const fetchRecordings = async () => {
+        const callData = await Promise.all(
+          callRecordings?.map((meeting) => meeting.queryRecordings()) ?? []
+        );
+
+        const recordings = callData
+          .filter((call) => call.recordings.length > 0)
+          .flatMap((call) => call.recordings);
+
+        setRecordings(recordings);
+      };
+
+      if (type === "recordings") {
+        fetchRecordings();
+      }
+    } catch (error) {
+      toast({ title: "Try Again Later" });
+    }
+  }, [type, callRecordings]);
   const calls = getCalls();
   const noCallsMessage = getNoCallsMessage();
   if (isLoading) return <Loader />;
